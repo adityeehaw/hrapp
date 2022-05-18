b551cb23e940d8bf33da85d7e250519b139cc591
Aditya <ataditiya@gmail.com>
2022-05-18 23:02:56 +0530

    back and frontend combine

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use(express.static('build'))
+
 let notes = [
     {
       id: 1,
@@ -27,9 +29,6 @@ let notes = [
     }
   ]
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
   
   app.get('/api/notes', (request, response) => {
     response.json(notes)
